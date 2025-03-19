d33e85e80c9878f2090fc129ac9a939400bfce22
Lv_Max <1048413433@qq.com>
2025-03-19 11:10:17 -0500

    version 15

--- patch.js
+++ patch.js
@@ -7,23 +7,23 @@ const patches = [
   {
     name: "Member info patch",
     target:
-      'var o=function\\(\\){var e;try{var t=m.get\\("_ot_v2_member"\\)\\|\\|{};return{mid:t.mid\\?parseInt\\(t.mid\\):null,email:null!==\\(e=t.email\\)&&void 0!==e\\?e:null,nickname:t.nick,subscriptions:t.subscriptions\\|\\|\\[\\],features:\\(t.subscriptions\\|\\|\\[\\]\\).reduce\\(\\(function\\(e,t\\){return t.expiry_at&&1e3\\*t.expiry_at>Date.now\\(\\)&&\\(e=r\\(r\\(\\[\\],e,!0\\),t.features\\|\\|\\[\\],!0\\)\\),e}\\),\\[\\]\\)}}catch\\(e\\){console.log\\(e\\)}return{mid:null,email:null,nickname:null,subscriptions:\\[\\],features:\\[\\]}\\}\\(\\);',
+      'var o=function(){var e;try{var t=m.get("_ot_v2_member")||{};return{mid:t.mid?parseInt(t.mid):null,email:null!==(e=t.email)&&void 0!==e?e:null,nickname:t.nick,subscriptions:t.subscriptions||[],features:(t.subscriptions||[]).reduce((function(e,t){return t.expiry_at&&1e3*t.expiry_at>Date.now()&&(e=i(i([],e,!0),t.features||[],!0)),e}),[])}}catch(e){console.log(e)}return{mid:null,email:null,nickname:null,subscriptions:[],features:[]}}();return n||(I.set("member",o),I.set("guest",H())),o}',
     replacement:
-      'var o={mid:4396,email:"HideOnBush",nickname:"JieJie",subscriptions:["premium"],features:["ad_free","premium_badge","lol_mypage","lol_favorites","lol_auto_record","lol_super_renew","tft_mypage","val_mypage","duo_pull_up","duo_highlight"]};',
+      'var o={mid:4396,email:"HideOnBush",nickname:"JieJie",subscriptions:["premium"],features:["ad_free","premium_badge","lol_mypage","lol_favorites","lol_auto_record","lol_super_renew","tft_mypage","val_mypage","duo_pull_up","duo_highlight"]};return n||(I.set("member",o),I.set("guest",!1)),o',
   },
   {
     name: "Logout function patch",
     target:
-      'function w\\(\\){m\\.delete\\("_ot"\\),m\\.delete\\("_ot_member"\\),m\\.delete\\("_ot_v2_refresh"\\),m\\.delete\\("_ot_v2_refresh_at"\\),m\\.delete\\("_ot_v2_member"\\),m\\.delete\\("_ot_v2_member_game_profile"\\),m\\.delete\\("_ot_guest"\\),I\\.delete\\("member"\\),b\\("logout",\\{opgg:!1,opggId:null\\}\\),S\\(\\)}',
+      'function w\\(\\)\\{m\\.delete\\("_ot"\\),m\\.delete\\("_ot_member"\\),m\\.delete\\("_ot_v2_refresh"\\),m\\.delete\\("_ot_v2_refresh_at"\\),m\\.delete\\("_ot_v2_member"\\),m\\.delete\\("_ot_v2_member_game_profile"\\),m\\.delete\\("_ot_guest"\\),I\\.delete\\("member"\\),f\\("logout",\\{opgg\\:!1,opggId\\:null\\}\\),S\\(\\)\\}',
     replacement:
-      'function w(){/*m.delete("_ot"),m.delete("_ot_member"),m.delete("_ot_v2_refresh"),m.delete("_ot_v2_refresh_at"),m.delete("_ot_v2_member"),m.delete("_ot_v2_member_game_profile"),m.delete("_ot_guest"),I.delete("member"),b("logout",{opgg:!1,opggId:null}),S()*/}',
+      'function w(){/*m.delete("_ot"),m.delete("_ot_member"),m.delete("_ot_v2_refresh"),m.delete("_ot_v2_refresh_at"),m.delete("_ot_v2_member"),m.delete("_ot_v2_member_game_profile"),m.delete("_ot_guest"),I.delete("member"),f("logout",{opgg:!1,opggId:null}),S()*/}',
   },
   {
     name: "Boot sequence patch",
     target:
-      "g.whenReady\\(\\).then\\(\\(function\\(\\){\\(f\\|\\|H\\(\\)\\)&&E\\(\\)}\\)\\)",
+      "d.whenReady\\(\\).then\\(\\(function\\(\\){\\(b\\|\\|H\\(\\)\\)&&N\\(\\)}\\)\\)",
     replacement:
-      'g.whenReady().then((function(){(f||H())&&E();var faker={mid:4396,email:"HideOnBush",nick:"JieJie",subscriptions:["premium"]};m.set("_ot_v2_member",faker);I.set("member",{mid:faker.mid,email:faker.email,nickname:faker.nick,subscriptions:faker.subscriptions,features:["ad_free","premium_badge","lol_mypage","lol_favorites","lol_auto_record","lol_super_renew","tft_mypage","val_mypage","duo_pull_up","duo_highlight"]});I.set("guest",!1);S()}))',
+      'd.whenReady().then((function(){(b||H())&&N();var faker={mid:4396,email:"HideOnBush",nick:"JieJie",subscriptions:["premium"]};m.set("_ot_v2_member",faker);I.set("member",{mid:faker.mid,email:faker.email,nickname:faker.nick,subscriptions:faker.subscriptions,features:["ad_free","premium_badge","lol_mypage","lol_favorites","lol_auto_record","lol_super_renew","tft_mypage","val_mypage","duo_pull_up","duo_highlight"]});I.set("guest",!1);N()}))',
   },
 ];
 
